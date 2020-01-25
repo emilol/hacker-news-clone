07be772d2aeb1cd6d389f7e8c850c927b40f187c
Emily Taylor <emilytaylor@live.com.au>
2020-01-25 10:19:13 +1000

    theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 import Posts from './components/Posts'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Nav />
+export default class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light',
+      }))
+    },
+  }
 
-        <Route exact path="/" render={() => <Posts type="top" />} />
-        <Route path="/new" render={() => <Posts type="new" />} />
-      </div>
-    </Router>
-  )
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+
+              <Route exact path="/" render={() => <Posts type="top" />} />
+              <Route path="/new" render={() => <Posts type="new" />} />
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+    )
+  }
 }
-
-export default App
--- src/components/PostMetaInfo.js
+++ src/components/PostMetaInfo.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ThemeConsumer } from '../contexts/theme'
 import { Link } from 'react-router-dom'
 
 export default function PostMetaInfo({ by, time, descendants }) {
   return (
-    <div className={`meta-info-light`}>
-      by <Link to={`/user/${by}`}>{by}</Link> on {new Date(time).toLocaleString()} with{' '}
-      {descendants} comments
-    </div>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className={`meta-info-${theme}`}>
+          by <Link to={`/user/${by}`}>{by}</Link> on {new Date(time).toLocaleString()} with{' '}
+          {descendants} comments
+        </div>
+      )}
+    </ThemeConsumer>
   )
 }
 
